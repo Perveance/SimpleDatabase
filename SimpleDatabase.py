87d9d08abf646300d175348299cbd20694ec4291
Perveance <mszhidko@gmail.com>
2016-04-30 16:12:52 -0700

    Implemented set, get, unset, numequalto methods

--- SimpleDatabase.py
+++ SimpleDatabase.py
@@ -17,65 +17,101 @@ class SimpleDatabase:
     def rollback(self):
         try:
             self._transStack.pop()
+            self._curTrans = self._transStack[len(self._transStack) - 1]
             return 0
         except IndexError:
             return "1"
 
-    def commit(self):
+    def _commit(self, merged):
         while (len(self._transStack) > 0):
             t = self._transStack.popleft()
             for var, val in t.getValues().iteritems():
-                self._values[var] = val
+                merged[var] = val
             for var in t.getUnsetVars():
-                del self._values[var]
+                del merged[var]
+
+    def commit(self):
+        self._commit(self._values)
+        self._curTrans = None
 
     def set(self, var, value):
-        pass
+        if self._curTrans:
+            self._curTrans.set(var, value)
+        else:
+            self._values[var] = value
 
     def unset(self, var):
-        pass
+        print "  -> unset Enter"
+        if self._curTrans:
+            print "  -> unset current transaction"
+            self._curTrans.unset(var)
+        else:
+            try:
+                del self._values[var]
+            except KeyError:
+                pass
+        print "  -> unset Exit"
+
 
     def get(self, var):
-        pass
+
+        ret = None
+        if self._curTrans:
+            tmpValues = dict(self._values)
+            self._commit(tmpValues)
+            try:
+                ret = tmpValues[var]
+            except:
+                ret = None
+        else:
+            try:
+                ret = self._values[var]
+            except KeyError:
+                ret = None
+        return ret
 
     def numEqualTo(self, value):
-        pass
-
-    def getValues(self):
-        pass
-
-    def getUnset(self):
-        pass
+        tmpRes = dict(self._values)
+        count = 0;
+        self._commit(tmpRes)
+        for var, val in tmpRes.iteritems():
+            if val == value:
+                count += 1
+        return count
 
 if __name__ ==  "__main__":
+
     db = SimpleDatabase()
 
+    db.set('b', '25')
     db.begin()
+    db.set('a', '10')
+    print "a = {0}".format(db.get('a'))
+    print "b = {0}".format(db.get('b'))
     db.begin()
+    db.set('a', '20')
+    print "second get a = {0}".format( db.get('a') )
+    print "b = {0}".format(db.get('b'))
+    db.rollback()
+    print "After rollback a should be 10. a = {0}".format(db.get('a'))
+    print "should be 25 b = {0}".format(db.get('b'))
     db.commit()
-    db.commit()
+    print "a = {0}".format(db.get('a'))
+    print "b = {0}".format(db.get('b'))
+    db.rollback() # This doesn't do anything
+    db.set('a', '30') # Not in transaction
+    db.set('b', '10')
+    print "(should be 30) a = {0}".format(db.get('a'))
+    print "(should be 10) b = {0}".format(db.get('b'))
+    #print "numequalto 10 = {0}".format(db.numEqualTo('10'))
+    db.begin() 
+    db.unset('a')
+    print "after unset should be none a = {0}".format(db.get('a'))
+    print "b = {0}".format(db.get('b'))
     db.rollback()
-    db.rollback()
-    db.rollback()
-
-    #t.set('a', '1')
-    #t.set('b', '3')
-    #t.set('c', '4')
-    #t.set('d', '11')
-    #t.set('e', '15')
-    #t.unset('e')
-    #t.unset('f')
-
-    #for var, val in t.getValues().iteritems():
-    #    print "{0} = {1}".format(var, val)
-
-    #print "Variable e equal to {0}".format( t.get('e') )
-    #print "Variable a equal to {0}".format( t.get('a') )
-    #print "Variable d equal to {0}".format( t.get('d') )
-    #print "Unset list: {0}".format( t.getUnset() )
-
-    #print "Variable {0} are equal to 3".format( t.numEqualTo('3') )
-    #print "Variable {0} are equal to 15".format( t.numEqualTo('15') )
-
-
+    print "should be 30 a = {0}".format(db.get('a'))
+    db.unset('a')
+    print "a = {0}".format(db.get('a'))
+    print "b = {0}".format(db.get('b'))
+    print "numequalto 10 = {0}".format(db.numEqualTo('10'))
 
--- Transaction.py
+++ Transaction.py
@@ -11,6 +11,7 @@ class Transaction:
 
     def unset(self, var):
         self._unsetList.append(var)
+        print self._unsetList
         try:
             del self._values[var]
         except KeyError:
@@ -23,15 +24,16 @@ class Transaction:
             return None
 
     def numEqualTo(self, value):
+        count = 0
         for var, val in self._values.iteritems():
             if val == value:
-                return var
-        return None
+                count += 1
+        return count
 
     def getValues(self):
         return self._values
 
-    def getUnset(self):
+    def getUnsetVars(self):
         return self._unsetList
 
 if __name__ ==  "__main__":
@@ -51,9 +53,8 @@ if __name__ ==  "__main__":
     print "Variable e equal to {0}".format( t.get('e') )
     print "Variable a equal to {0}".format( t.get('a') )
     print "Variable d equal to {0}".format( t.get('d') )
+    print "Unset list: {0}".format( t.getUnsetVars() )
 
     print "Variable {0} are equal to 3".format( t.numEqualTo('3') )
     print "Variable {0} are equal to 15".format( t.numEqualTo('15') )
-
-
 
